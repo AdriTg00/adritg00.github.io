c88773dc116c6236102912338aa01275838ae789
AdriTg00 <adrian2000tg@gmail.com>
2024-10-30 12:49:32 +0100

    Add files via upload

---
+++ HTML_ADRI/envio.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+Hola <?php echo $_POST["nombre"]; ?> !<br>
+
+Tu correo es <?php echo $_POST["correo"]; ?>
+
+Has enviado el siguiente mensaje: <p> <?php echo $_POST["mensaje"]; ?> </p>
+
+Tu respuesta a si deseas recibir respuesta:  <?php echo $_POST["envioEmail"]; ?> 
+
+La opcion es: <?php echo $_POST["opcionSeleccionada"]; ?> 
+</body>
+</html>
